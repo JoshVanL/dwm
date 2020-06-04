c4290cf8b9dab978a4ab0dec6554a7b9cee79a90
JoshVanL <vleeuwenjoshua@gmail.com>
2020-06-04 22:30:29 +0100

    Add vacant tags
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- config.h
+++ config.h
@@ -13,10 +13,10 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Arial:size=27" };
-static const char dmenufont[]       = "Menlo for Powerline:size=27";
+static const char *fonts[]          = { "FontAwesome, Arial:size=33" };
+static const char dmenufont[]       = "FontAwesome, Menlo for Powerline:size=33";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#444444"; // TODO: make transparent
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_gray5[]       = "#000000";
@@ -28,6 +28,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+static const char *alttags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -68,10 +69,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray1, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray1, "-sf", col_gray4, "-l", "5", NULL };
 static const char *termcmd[]  = { "termite", NULL };
 static const char *chromiumcmd[]  = { "chromium", "--force-device-scale-factor=1.75", "--force-dark-mode", NULL };
 static const char *roficmd[]  = { "rofi", "-font", "Roboto 26", "-show", "run", NULL };
+static const char *lockcmd[]  = { "/home/josh/scripts/lock.sh", "/home/josh/img/wallpapers/3-lock.png", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -85,7 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_j,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_k,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_n,      setlayout,      {.v = &layouts[0]} }, // set to tile
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} }, // set to monocle
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} }, // set to grid
@@ -107,11 +109,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	TAGKEYS(                        XK_0,                      9)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_q,      quit,           {0} },
 
   // command keys
 	{ MODKEY|ShiftMask,             XK_c,      spawn,          {.v = chromiumcmd } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
+	{ MODKEY|ShiftMask,             XK_Delete, spawn,          {.v = lockcmd } },
 };
 
 /* button definitions */
